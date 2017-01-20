018fdcaf1cca886f191febf6bd10ea7b15f0b6c4
Collin Mutembei <mutembei.collin@gmail.com>
2017-01-20 21:36:58 +0300

    Complete project

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,6 +6,28 @@ $(function() {
     dataType: 'jsonp',
     success: function(response) {
       // handle response
+      var $badges = $('#badges');
+
+      courses.forEach(function(course) {
+        $div = $('<div />', {
+          'class': 'course'
+        }).appendTo($badges)
+      })
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($div);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($div);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($div)
     }
   });
 
