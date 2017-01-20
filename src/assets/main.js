0aa51acf0bb8369d0eb5954aefa3c66148a9df11
Collin Mutembei <mutembei.collin@gmail.com>
2017-01-20 19:53:29 +0300

    Make an Ajax Call

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,12 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/2319950.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+    }
+  });
 
 });
